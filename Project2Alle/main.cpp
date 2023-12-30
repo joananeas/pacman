4146319f9ec5861f197b41780da98ef62d0db94d
nasneas <joananeasamoraga@gmail.com>
2023-12-30 18:32:28 +0100

    v0.1.0 - tablero UI hecho

--- Project2Alle/main.cpp
+++ Project2Alle/main.cpp
@@ -1,5 +1,4 @@
 // Hecho por: Husnain Arsha, Ivan Aguilar y Joan Aneas :)
-// v0.0.2
 
 #include <iostream>
 #include <string>
@@ -19,12 +18,24 @@ int jugar();
 int menu();
 int ancho = 1024;
 int alto = 768;
+const char* version = "v0.1.0"; // Tiene que ser const char* para que funcione con al_draw_text
 
 ALLEGRO_DISPLAY* ventana;
 ALLEGRO_FONT* hello_honey;
 ALLEGRO_TIMER* segundoTimer;
 ALLEGRO_TIMER* fps;
 ALLEGRO_EVENT_QUEUE* event_queue;
+
+void finalizar_allegro() {
+	// Destruir el temporizador
+	al_destroy_timer(segundoTimer);
+
+	// Destruir la cola de eventos
+	al_destroy_event_queue(event_queue);
+
+	// Otros códigos de limpieza pueden ir aquí
+}
+
 
 int main() {
 
@@ -73,19 +84,110 @@ int main() {
 	return 0;
 }
 
-int jugar()
-{
+void dibujar_tablero() {
+	ALLEGRO_COLOR colorPared = al_map_rgb(0, 0, 255); // Azul
+	ALLEGRO_COLOR colorPasillo = al_map_rgb(0, 0, 0); // Negro
 
-	while (true)
-	{
+	// Dimensiones de la ventana
+	int ventanaAncho = 1024;
+	int ventanaAlto = 768;
+
+	// Dimensiones del tablero
+	int tableroAncho = 900;
+	int tableroAlto = 600;
+
+	// Calcular las coordenadas iniciales para centrar el tablero
+	int inicioX = (ventanaAncho - tableroAncho) / 2; // 62
+	int inicioY = (ventanaAlto - tableroAlto) / 2; // 84
+
+	int dentroRectX = inicioX + 30; // contando grosor
+	int dentroRectY = inicioY + 30;
+
+	al_draw_filled_rectangle(inicioX, inicioY, inicioX + 900, inicioY + 30, colorPared); // Tope superior
+	al_draw_filled_rectangle(inicioX + 870, inicioY, inicioX + 900, inicioY + 600, colorPared); // Tope derecha 
+	al_draw_filled_rectangle(inicioX, inicioY, inicioX + 30, inicioY + 600, colorPared); // Tope izquierdo
+	al_draw_filled_rectangle(inicioX, inicioY + 570, inicioX + 900, inicioY + 600, colorPared); // Tope inferior
+	
+	// Obstáculos
+
+	// Lateral izquierda
+	al_draw_filled_rectangle(dentroRectX + 60, dentroRectY + 60, dentroRectX + 210, dentroRectY + 120, colorPared);
+	
+	// Central superior
+	al_draw_filled_rectangle(dentroRectX + 270, dentroRectY + 60, dentroRectX + 570, dentroRectY + 120, colorPared);
+
+	// Lateral derecha
+	al_draw_filled_rectangle(dentroRectX + 630, dentroRectY + 60, dentroRectX + 780, dentroRectY + 120, colorPared);
+
+	// Lateral L izquierda
+	al_draw_filled_rectangle(dentroRectX + 60, dentroRectY + 180, dentroRectX + 210, dentroRectY + 240, colorPared);
+	al_draw_filled_rectangle(dentroRectX + 150, dentroRectY + 180, dentroRectX + 210, dentroRectY + 360, colorPared);
+	
+	// Inferior izquierda
+	al_draw_filled_rectangle(dentroRectX + 60, dentroRectY + 420, dentroRectX + 210, dentroRectY + 480, colorPared);
+
+	// Rectángulo central (fantasmas)
+	al_draw_filled_rectangle(dentroRectX + 270, dentroRectY + 180, dentroRectX + 570, dentroRectY + 240, colorPared);
+	al_draw_filled_rectangle(dentroRectX + 270, dentroRectY + 180, dentroRectX + 330, dentroRectY + 360, colorPared);
+	al_draw_filled_rectangle(dentroRectX + 270, dentroRectY + 300, dentroRectX + 570, dentroRectY + 360, colorPared);
+	al_draw_filled_rectangle(dentroRectX + 510, dentroRectY + 180, dentroRectX + 570, dentroRectY + 360, colorPared);
+
+	// Lateral L derecha
+	al_draw_filled_rectangle(dentroRectX + 630, dentroRectY + 180, dentroRectX + 780, dentroRectY + 240, colorPared);
+	al_draw_filled_rectangle(dentroRectX + 630, dentroRectY + 180, dentroRectX + 690, dentroRectY + 360, colorPared);
+
+	// Central inferior
+	al_draw_filled_rectangle(dentroRectX + 270, dentroRectY + 420, dentroRectX + 570, dentroRectY + 480, colorPared);
+
+	// Inferior derecha
+	al_draw_filled_rectangle(dentroRectX + 630, dentroRectY + 420, dentroRectX + 780, dentroRectY + 480, colorPared);
+
+
+
+	float grosor = 30;
+	// Dibujar texto sobre el tablero
+	ALLEGRO_FONT* font = al_create_builtin_font();
+	if (!font) {
+		fprintf(stderr, "Error al cargar la fuente incorporada.\n");
+		return;
+	}
+
+	float puntos = 100;
+
+	/*--------------------------------------------------------------------------------------------------------*/
+	/**/al_draw_text(font, al_map_rgb(255, 255, 255), inicioX + 42, 50, ALLEGRO_ALIGN_CENTER, "Puntos: 10");/**/
+	/**/al_draw_text(font, al_map_rgb(255, 255, 255), 512, 50, ALLEGRO_ALIGN_CENTER, "Vidas: 3");/*-----------*/
+	/**/al_draw_text(font, al_map_rgb(255, 255, 255), inicioX + 875, 50, ALLEGRO_ALIGN_CENTER, version);/*----*/
+	/*--------------------------------------------------------------------------------------------------------*/
+
+	al_destroy_font(font);
+}
+
+int jugar() {
+	ALLEGRO_DISPLAY* display = al_create_display(1024, 768);
+
+	if (!display) {
+		printf("[ERROR] No se pudo crear la ventana.\n");
+		return -1;
+	}
+
+	while (true) {
 		ALLEGRO_EVENT Evento;
 		al_wait_for_event(event_queue, &Evento);
-		al_clear_to_color(al_map_rgb(0, 0, 0));
-		al_draw_filled_circle(200, 200, 100, al_map_rgb(255, 255, 255));
-		al_flip_display();
+
+		if (Evento.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+			break; // Salir del bucle si el usuario cierra la ventana
+		}
+
+		al_clear_to_color(al_map_rgb(0, 0, 0)); // Limpia la pantalla a negro
+		// Dibujar el tablero al estilo de Pac-Man
+		dibujar_tablero();
+		al_flip_display(); // Actualizar la pantalla
 	}
+	al_destroy_display(display); // Liberar recursos al salir
 	return 1;
 }
+
 int menu() {
 	int segundos = 0;
 	int x = -1, y = -1;
@@ -153,4 +255,7 @@ int menu() {
 
 		al_flip_display();
 	}
+	finalizar_allegro();
+
+	return 0;
 }
